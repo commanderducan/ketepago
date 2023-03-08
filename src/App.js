be9ab9858686cf42dfd8ceab2901ebfd0583b0d2
commanderducan <ssswhitesss1@gmail.com>
2023-03-08 19:47:12 +0100

    v1 componente app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";  
 
-function App() {
-  const [valor, setValor] = useState(0)
+function App() {   
 
+  const [cantidad, setCantidad] = useState(0)   
+  const [personas, setPersonas] = useState(0)
+  const [parte, setParte] = useState(0)
+  const [elementosHTML, setElementosHTML] = useState([])
+
+  function manejadorInputCantidad(evento) {  
+    const nuevoValor = evento.target.value
+    setCantidad(nuevoValor)
+  }
+
+  function manejadorInputPersonas(evento) {
+    const nuevoValor = evento.target.value
+    setPersonas(nuevoValor)
+  }
+
+  useEffect (
+    ()=>{
+      setParte(cantidad/personas)
+    },
+    [cantidad, personas]
+  )
+
+  useEffect (
+    ()=>{
+      const nuevosElementos = []
+      let contador = 0
+      while (contador < personas) {
+          nuevosElementos.push(<p>Un parrafo {parte} </p>)
+          contador++
+      }
+      setElementosHTML(nuevosElementos)
+    },
+    [personas]
+  )
 
   return (
       <>
         <label>
           Cantidad:
-          <input type="text"/>
-
+          <input onInput={manejadorInputCantidad} value={cantidad} type="text"/>
         </label>
+        <br/>
 
         <label> 
           Personas:
-          <input type="range"/>
+          <input onInput={manejadorInputPersonas} value={personas} type="range"/>
         </label>
+        {personas}
+        <br/>
+        {elementosHTML}
       </>
-  );
+  )
 }
 
-export default App;
+export default App;G
+
+
+
+
